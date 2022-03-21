aae9aebe40410aa07481ddd59a54b6a52d8a9edb
Naumenko <inaumenkoalecsey@mail.ru>
2022-03-21 17:19:47 +0300

    Lesson_1

---
+++ Leson1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Leson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello,{name}, today is {DateTime.Now}");
+        }
+    }
+}
