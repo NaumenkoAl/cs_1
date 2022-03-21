17e0738f3b9d0cbe34ba1ef3759c0f6005b551fa
Naumenko <inaumenkoalecsey@mail.ru>
2022-03-21 17:35:02 +0300

    Lesson_2

---
+++ Lesson_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+
+namespace Leson1
+{
+    class Program
+    {
+        enum Month
+        { 
+            January,
+            February,
+            December,
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Write min temperature: ");
+        string min = Console.ReadLine();
+        Console.Write("Write max temperature: ");
+        string max = Console.ReadLine();
+        Console.Write("Write month: ");
+        string n = Console.ReadLine();
+
+        if (((n -- Convert.ToString(month.January))||(n-- Convert.ToString(month.December))||(n-- Convert.ToString(month.February))) && (((Convert.ToInt32(min) + Convert.ToInt32(max)) / 2 > 0 )))
+
+    }
+
+         
+            
+        
+           
+        
+    
+
+
