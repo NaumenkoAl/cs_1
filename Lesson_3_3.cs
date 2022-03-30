e20a1625031479e7ede251f2bfb3f7e49c36f8a3
Naumenko <inaumenkoalecsey@mail.ru>
2022-03-30 15:24:51 +0300

    Lesson_3_2.3_3

---
+++ Lesson_3_3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку : Hello");
+            string str = Console.ReadLine();
+            char[] s = str.ToCharArray();
+            str = "";
+
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+
+                str += s[i];
+            }
+
+            Console.WriteLine();
+            Console.Write("Result :");
+            Console.WriteLine(str);
+
+
+
+            Console.ReadLine();
+        }
+    }    
+}
