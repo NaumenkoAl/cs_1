4671b2bfd1a50028dce68d5a7cddf767139e1766
Naumenko <inaumenkoalecsey@mail.ru>
2022-03-30 15:16:15 +0300

    Lesson_3

---
+++ Lesson_3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Put number: ");
+        int a = int.Parse(Console.ReadLine());
+        int k = 1;
+        int[,] mas = new int[a, a];
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < a; j++)
+            {
+                if (i == j) mas[i, j] = k;
+                Console.Write(mas[i, j] + "  ");
+            }
+            k++;
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
